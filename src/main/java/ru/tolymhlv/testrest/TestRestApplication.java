b3bdf0c9f54479b360aff86edd696d5feb2c5bbc
uncletolya <stolikzakazan@gmail.com>
2019-04-01 19:50:45 +0300

    add structure

---
+++ src/main/java/ru/tolymhlv/testrest/TestRestApplication.java
@@ -0,0 +1,12 @@
+package ru.tolymhlv.testrest;
+
+import com.apple.eawt.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
