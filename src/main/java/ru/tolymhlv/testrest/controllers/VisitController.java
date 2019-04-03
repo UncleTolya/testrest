20adec1fa6e1b05eac67f756ef0d32885b16ab4e
uncletolya <stolikzakazan@gmail.com>
2019-04-03 23:02:30 +0300

    controller refactor

--- src/main/java/ru/tolymhlv/testrest/controllers/VisitController.java
+++ src/main/java/ru/tolymhlv/testrest/controllers/VisitController.java
@@ -27,15 +27,15 @@ public class VisitController {
 
     @PostMapping(name = "/visit")
     public String makeVisitEventAndGetCommonStatistic(
-            final @RequestParam String userId,
-            final @RequestParam String pageId,
+            final @RequestParam(name = "userId") String userId,
+            final @RequestParam(name = "pageId") String pageId,
             final Model model) {
 
         visitService.addVisit(userId, pageId);
 
         final List<Visit> visitsToday = visitService.getVisitsFromStartDay();
-        final Integer counterVisitsToday = visitsToday.size();
-        final Long counterUniqUsersToday = visitsToday
+        final int counterVisitsToday = visitsToday.size();
+        final long counterUniqUsersToday = visitsToday
                 .stream()
                 .map(Visit::getUserId) // got the List<UserId>
                 .distinct() // delete duplicate UserId from the list
@@ -49,26 +49,26 @@ public class VisitController {
     @RequestMapping(value = "/visits/{from}/{to}", method = RequestMethod.GET)
     public String getStatisticByDate(
             final @PathVariable(name = "from") String from,
-            final @PathVariable String to,
+            final @PathVariable(name = "to") String to,
             final Model model) {
 
         final LocalDateTime fromAsTime = dateAndTimeUtils.stringToTime(from);
         final LocalDateTime toAsTime = dateAndTimeUtils.stringToTime(to);
 
         final List<Visit> visitsByDate = visitService.getVisitsBetweenDates(fromAsTime, toAsTime);
-        final Integer counterVisitsByDate = visitsByDate.size();
-        final Long counterUniqUsersByDate = visitsByDate
+        final int counterVisitsByDate = visitsByDate.size();
+        final long counterUniqUsersByDate = visitsByDate
                 .parallelStream()
                 .map(Visit::getUserId)
                 .distinct()
                 .count();
-        final Long counterUniqRegularUsersByDate = visitsByDate
+        final long counterUniqRegularUsersByDate = visitsByDate
                 .parallelStream()
-                .collect(Collectors.groupingByConcurrent(Visit::getUserId)) // got ConcurrentMap<UserId, List<Visit>>
+                .collect(Collectors.groupingByConcurrent(Visit::getUserId))
                 .entrySet()
                 .parallelStream()
-                .map(Map.Entry::getValue) //got List<List<Visit>>, each one belong to uniq user
-                .filter(visits -> visits.size() >= 10) // filter visits by quantity condition from the task
+                .map(Map.Entry::getValue)
+                .filter(visits -> visits.size() >= 10)
                 .count();
 
         model.addAttribute("counterVisitsByDate", counterVisitsByDate);
